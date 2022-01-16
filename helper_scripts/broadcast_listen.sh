1f496ac86f5228591414427a1289932052aa26cb
starwader <starwader@gmail.com>
2022-01-16 17:19:14 +0100

    Delete helper scripts

--- helper_scripts/broadcast_listen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#example ./broadcast_listen.sh 12345
-nc -lu -p $1
--- helper_scripts/send_broadcast.py
+++
@@ -1,5 +0,0 @@
-from broadcast_server import broadcast
-from src.const import *
-
-broadcast("broadcast message %s" % BROADCAST_PORT, BROADCAST_PORT)
-
