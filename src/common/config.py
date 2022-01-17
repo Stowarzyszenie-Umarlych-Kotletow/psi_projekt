30f9b65b55d1fb8a4e6d3fe8fc7c76cc94e73f55
tedtheripper <lemon.juice0731@gmail.com>
2022-01-17 22:41:30 +0100

    Added logger to the repository

--- src/common/config.py
+++ src/common/config.py
@@ -1,4 +1,5 @@
 import os
+
 # Unicast and broadcast ports are the same in the whole network
 # BROADCAST_IP must be set the same in the whole network
 # if you experience problems with
@@ -7,9 +8,9 @@ import os
 BROADCAST_PORT = 12345
 UNICAST_PORT = 12346
 TCP_PORT = 13371
-BROADCAST_IP = '<broadcast>'  # default
+BROADCAST_IP = "<broadcast>"  # default
 # BROADCAST_IP = '192.168.80.255'
-UNICAST_IP = '0.0.0.0'
+UNICAST_IP = "0.0.0.0"
 FINDING_TIME = 2
 SEARCH_RETRIES = 2
 
@@ -17,10 +18,12 @@ SEARCH_RETRIES = 2
 DEBUG = True
 BROADCAST_OMIT_SELF = True
 PROTO_VERSION = 1
-ENCODING = 'utf-8'
-MAGIC_NUMBER = 0xd16d
+ENCODING = "utf-8"
+MAGIC_NUMBER = 0xD16D
 UDP_BUFFER_SIZE = 2048
 MAX_FILENAME_LENGTH = 27
+METADATA_FOLDER_NAME = ".meta"
+YAML_EXTENSION = ".yaml"
 MOCK_CONTROLLER_PATH = os.getcwd()
-DIGEST_ALG = 'sha256'
+DIGEST_ALG = "sha256"
 FINGERPRINT_LENGTH = 10
